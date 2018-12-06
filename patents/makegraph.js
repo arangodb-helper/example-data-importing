e701d1ed473d4db46e32684beef4a5d3e9a8837a
Max Neunhoeffer <max@arangodb.com>
2018-12-06 16:18:19 +0100

    Graph wrongly generated as smart graph.

--- patents/makegraph.js
+++ patents/makegraph.js
@@ -6,5 +6,5 @@ let G = g._create("G_smart",[g._relation("citations_smart",
 g = require("@arangodb/general-graph");
 G = g._create("G_naive",[g._relation("citations_naive",
                                ["patents_naive"],["patents_naive"])],
-              [], {numberOfShards:3, smartGraphAttribute:"COUNTRY"});
+              [], {numberOfShards:3});
 
